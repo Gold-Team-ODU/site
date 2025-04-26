38dc0ab9a895505ebf10eda09f63af52c13ba4ca
Gabriel Lugo <glugo001@odu.edu>
2025-04-25 22:27:17 -0400

    Fix nav and footer links

--- site/scripts/footer.js
+++ site/scripts/footer.js
@@ -1,14 +1,36 @@
-fetch("footer.html")
+function getRoot() {
+  const hostname = window.location.hostname;
+  const pathname = window.location.pathname;
+
+  if (hostname.endsWith("github.io")) {
+    const pathParts = pathname.split('/');
+    if (pathParts.length > 1 && pathParts[1] !== "") {
+      return `/${pathParts[1]}/`; // repo name
+    } else {
+      return "/"; // default root
+    }
+  } else {
+    return "/"; // default root
+  }
+}
+
+function setSrc(id) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.src = getRoot() + id;
+  }
+}
+
+fetch("../footer.html")
   .then(response => {
     if (response.ok) {
-      let text = response.text();
-      console.debug("[GOT]", text);
-      return text;
+      return response.text();
     }
     throw new Error("Failed to load footer");
   })
   .then(footer => {
     document.getElementById("footer").innerHTML = footer;
+    setSrc("assets/images/odu_cs.jpg");
   })
   .catch(error => {
     console.error("[ERROR]", error);
--- site/scripts/nav.js
+++ site/scripts/nav.js
@@ -1,4 +1,27 @@
-fetch("nav.html")
+function getRoot() {
+  const hostname = window.location.hostname;
+  const pathname = window.location.pathname;
+
+  if (hostname.endsWith("github.io")) {
+    const pathParts = pathname.split('/');
+    if (pathParts.length > 1 && pathParts[1] !== "") {
+      return `/${pathParts[1]}/`; // repo name
+    } else {
+      return "/"; // default root
+    }
+  } else {
+    return "/"; // default root
+  }
+}
+
+function setLink(id) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.href = getRoot() + id;
+  }
+}
+
+fetch("../nav.html")
   .then(response => {
     if (response.ok) {
       let text = response.text();
@@ -7,8 +30,17 @@ fetch("nav.html")
     }
     throw new Error("Failed to load navbar");
   })
-  .then(footer => {
-    document.getElementById("nav").innerHTML = footer;
+  .then(nav => {
+    document.getElementById("nav").innerHTML = nav;
+
+    setLink("index");
+    setLink("customers");
+    setLink("presentations/societal_problem");
+    setLink("presentations/feasibility");
+    setLink("presentations/design");
+    setLink("lab1_outline");
+    setLink("references");
+    setLink("team");
   })
   .catch(error => {
     console.error("[ERROR]", error);
